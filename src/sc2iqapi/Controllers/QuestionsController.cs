512ca130689de1275164d0572c806cc2edb9a848
Matt Mazzola <mattmazzola@live.com>
2015-11-28 16:16:48 -0800

    Update question controller to support basic crud operations on question. Add question model validation.

---
+++ src/sc2iqapi/Controllers/QuestionsController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using sc2iqapi.Models;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace sc2iqapi.Controllers
+{
+    [Route("api/[controller]")]
+    public class QuestionsController : Controller
+    {
+        [FromServices]
+        public Sc2IqContext DbContext { get; set; }
+
+        // GET: api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var questions = DbContext.Questions;
+
+            return Json(questions);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var question = DbContext.Questions.FirstOrDefault(q => q.Id == id);
+
+            return Json(question);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]Question question)
+        {
+            if(!ModelState.IsValid)
+            {
+                return HttpBadRequest(ModelState);
+            }
+
+            DbContext.Questions.Add(question);
+            await DbContext.SaveChangesAsync();
+
+            return Json(question);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var question = DbContext.Questions.FirstOrDefault(q => q.Id == id);
+            if(question == null)
+            {
+                return HttpNotFound();
+            }
+
+            DbContext.Remove(question);
+
+            await DbContext.SaveChangesAsync();
+
+            return Json(id);
+        }
+    }
+}
--- src/sc2iqapi/Models/Question.cs
+++ src/sc2iqapi/Models/Question.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,14 +9,36 @@ namespace sc2iqapi.Models
     public class Question
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(256)]
         public string Q { get; set; }
+
+        [Required]
+        [MaxLength(64)]
         public string A1 { get; set; }
+
+        [Required]
+        [MaxLength(64)]
         public string A2 { get; set; }
+
+        [Required]
+        [MaxLength(64)]
         public string A3 { get; set; }
+
+        [Required]
+        [MaxLength(64)]
         public string A4 { get; set; }
+
+        [Required]
         public int Difficulty { get; set; }
+
+        [Required]
         public int CorrectAnswerIndex { get; set; }
+
         public QuestionState State { get; set; }
+
+        [Required]
         public int CreatedBy { get; set; }
     }
 
